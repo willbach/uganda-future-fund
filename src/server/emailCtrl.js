2d0c867fd9f1221482763145acbdcd765e8628ff
willbach <willbach@gmail.com>
2017-01-23 11:17:51 -0800

    contact working and home pictures rotating

---
+++ src/server/emailCtrl.js
@@ -0,0 +1,43 @@
+//put everything necessary to send emails in this file
+
+var nodemailer = require('nodemailer');
+
+const user = '';
+const password = '';
+const sender = 'TUFF Contact Request';
+
+var smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: user,
+        pass: password
+    }
+};
+
+var transport = nodemailer.createTransport(smtpConfig);
+
+const emailCtrl = {};
+
+emailCtrl.contactUs = function(contactInfo) {
+    var mailOptions = {
+        from: sender,
+        to: 'william.galebach@gmail.com',
+        subject: 'Uganda Future Fund contact request', // Subject line
+        text: `${contactInfo.Name} at ${contactInfo.Email} submitted a contact request:
+        
+        Subject: ${contactInfo.Subject}
+
+        Message: ${contactInfo.Comment}`
+    };
+    transport.sendMail(mailOptions, function(error, info) {
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    });
+}
+
+module.exports = emailCtrl;
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const emailCtrl = require('./emailCtrl.js');
 
 const app = express();
 
 const PORT = 2000;
 
+app.use(bodyParser.urlencoded({extended: true}), bodyParser.json());
 app.use(express.static(path.join(__dirname, './../client')));
 
-
+app.post('/contact', (req, res) => {
+  emailCtrl.contactUs(req.body);
+  res.redirect('/#contact');
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
